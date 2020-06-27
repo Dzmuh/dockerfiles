0a395e0ce4371e1c13695624d718e92a57a0079a
Ilya Fralkou <dzmuh@outlook.com>
2020-06-27 04:52:58 +0300

    С гимпом пока сложности

---
+++ gimp/run.sh
@@ -0,0 +1,8 @@
+docker run --rm -it --name gimp \
+    --hostname=$(hostname) \
+    --device /dev/snd \
+    -e DISPLAY \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v "/home/$(whoami)/.Xauthority:/home/user/.Xauthority" \
+    -v /etc/localtime:/etc/localtime:ro \
+    local/gimp
